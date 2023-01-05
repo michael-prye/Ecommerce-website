bfd32e0615bf0dfb191fbe11004a806c08801732
michael-prye <michael.prye@gmail.com>
2023-01-05 15:52:19 -0500

    Removed Account App + Created Address APP + added models to Address App

---
+++ backend/address/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from authentication.models import User
+
+
+# Create your models here.
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    zip_code = models.IntegerField()
+
