5486e7b0fb8c3f2e80be3573a78f0b7f875f9659
michael-prye <michael.prye@gmail.com>
2023-01-05 16:20:16 -0500

    Created and finalized Address serializer + Updated address model

--- backend/address/models.py
+++ backend/address/models.py
@@ -4,10 +4,9 @@ from authentication.models import User
 
 # Create your models here.
 
-class Account(models.Model):
+class Address(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
     zip_code = models.IntegerField()
-
