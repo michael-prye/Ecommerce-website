ba3469c2aaefc287f2118524c6b802adebe1fbf4
michael-prye <michael.prye@gmail.com>
2023-01-16 17:25:10 -0500

    Updated Urls for Address app and project Urls + removed query params for get request + Successful Postman GET request

--- backend/address/urls.py
+++ backend/address/urls.py
@@ -1,6 +1,6 @@
-from django import path
+from django.urls import path
 from address import views
 
 urlpatterns= [
-    path('', views.get_address)
+    path('', views.get_address),
 ]
--- backend/address/views.py
+++ backend/address/views.py
@@ -10,14 +10,11 @@ from .serializers import AddressSerializer
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_address(request):
-    address_id = request.query_parms.get('id')
+   # address_id = request.query_parms.get('id')
     queryset = Address.objects.all()
 
-    if address_id:
-        queryset = queryset.filter(address_id = address_id)
-        serializer = AddressSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
-        queryset = queryset.filter(user_id=request.user.id)
-        serializer = AddressSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    queryset = queryset.filter(user_id=request.user.id)
+    serializer = AddressSerializer(queryset, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+    
