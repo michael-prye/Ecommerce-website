21269e72f8add44c657865dbde03816923d981d6
michael-prye <michael.prye@gmail.com>
2023-01-18 11:42:29 -0500

    Added Caregory class to Product Models + Added category ForeignKey to Product class

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+
+class Product(models.Model):
+    name =models.CharField(max_length=255)
+    description=models.CharField(max_length=255)
+    price=models.DecimalField(max_digits=5, decimal_places=2)
+    category=models.ForeignKey(Category)
+
+
+
+
