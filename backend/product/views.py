ae96804ac0c7f373a5dda03b8b174a860296933f
michael-prye <michael.prye@gmail.com>
2023-01-20 12:09:39 -0500

    Finalized Post request for Product + Successfull request in Postman

--- backend/product/views.py
+++ backend/product/views.py
@@ -22,13 +22,21 @@ def product_category(request):
         serializer = CategorySerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 @permission_classes([IsAuthenticated])
 def product_list(request):
     if request.method == 'GET':
         queryset = Product.objects.all()
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'POST':
+        category_id = request.query_params.get('id')
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(category_id=category_id)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
         
