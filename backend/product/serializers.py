c544c39ff3103eb0da2ecaa730ba2643a7cd509d
michael-prye <michael.prye@gmail.com>
2023-01-20 11:48:21 -0500

    Finalized ProductImage Model + Finalized imageSerializer

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
-from .models import Product, Category
+from .models import Product, Category, ProductImage
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=Category
         fields = ['id', 'name']
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=ProductImage
+        fields = ['id', 'name', 'product','image','default']
+        depth = 1
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
