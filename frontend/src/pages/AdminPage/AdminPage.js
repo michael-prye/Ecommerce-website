0690fa6e1d7a9e68cccbc44a8db0e135dca29e12
michael-prye <michael.prye@gmail.com>
2023-02-10 12:18:48 -0500

    Added switch case for admin site

--- frontend/src/pages/AdminPage/AdminPage.js
+++ frontend/src/pages/AdminPage/AdminPage.js
@@ -1,7 +1,29 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
 const AdminPage = () => {
+    const [adminChoice, setAdminChoice] = useState('productList')
+
+    let adminComponent;
+    switch(adminChoice){
+        case 'newProduct':
+            adminComponent = <h1>New Product</h1>
+            break;
+        case 'productList':
+            adminComponent = <h1>Product List</h1>
+            break;
+    }
+
+
+
     return(
         <div>
-            <a>ADMIN PAGE</a>
+            <ul>
+            <li className={adminChoice === 'ProductList' ? 'tab--active' : 'tab--inactive'} onClick={()=>setAdminChoice('productList')} >Product List</li>
+            <li className={adminChoice === 'newProduct' ? 'tab--active' : 'tab--inactive'} onClick={()=>setAdminChoice('newProduct')} >New Product</li>
+            
+            </ul>
+            {adminComponent}
         </div>
     )
 };
